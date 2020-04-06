c4b795ee01b2d36162a564ce891c9ab22c0ed6da
AndoniLuna <andoniluna22@hotmail.com>
2020-04-06 17:41:53 +0200

    Actividad 7 y ejercicios de teoria map, filter y reduce

--- appclient/js/pokemon.js
+++ appclient/js/pokemon.js
@@ -11,6 +11,21 @@ function pintarLista(){
     const urlLista = 'https://pokeapi.co/api/v2/pokemon/';
     console.trace('pintarLista');
     
+    const promesa = ajax('GET', urlLista, undefined );
+    promesa.then( (data) => {
+        const pokemons = data.results;
+
+        let lista = document.getElementById('lista');
+        lista.innerHTML = '';
+
+        pokemons.forEach( el => {
+            lista.innerHTML += `<li onclick="detallePokemon('${el.name}')" 
+                                    class="list-group-item">${el.name}</li>`;
+        });
+    });
+    promesa.catch( (error) => alert(error));
+
+    /*
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         
@@ -19,27 +34,19 @@ function pintarLista(){
             // parsear texto a Json
             const jsonData = JSON.parse(this.responseText);    
             console.debug( jsonData );
-
             // array con personas
             const pokemons = jsonData.results;
-
             let lista = document.getElementById('lista');
             lista.innerHTML = '';
-
             pokemons.forEach( el => {
                 lista.innerHTML += `<li onclick="detallePokemon('${el.name}')" 
                                         class="list-group-item">${el.name}</li>`;
             });
-
-
         }// his.readyState == 4 && this.status == 200
-
     };// onreadystatechange
-
     xhttp.open("GET", urlLista , true);
     xhttp.send();
-
-
+    */
     
 
 
@@ -50,28 +57,34 @@ function detallePokemon( nombre ){
     console.trace(`detallePokemon ${nombre}`);
     const urlDetalle = `https://pokeapi.co/api/v2/pokemon/${nombre}/`;
 
+    ajax('GET', urlDetalle, undefined )
+        .then( jsonData =>{
+            let elNombre = document.getElementById('pokemonNombre');
+            elNombre.textContent = jsonData.name;
+           
+            let elImagen = document.getElementById('pokemonImagen');
+            elImagen.src = jsonData.sprites.front_default;
+        }).catch( error => {
+            alert(error);
+        });
+
+    /*
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         
         if (this.readyState == 4 && this.status == 200) {
             const jsonData = JSON.parse(this.responseText);    
             console.debug( jsonData );
-
             let elNombre = document.getElementById('pokemonNombre');
             elNombre.textContent = jsonData.name;
            
             let elImagen = document.getElementById('pokemonImagen');
             elImagen.src = jsonData.sprites.front_default;
-
-
-
         }// his.readyState == 4 && this.status == 200
-
     };// onreadystatechange
-
     xhttp.open("GET", urlDetalle , true);
     xhttp.send();
-
+    */
    
 
 }//detallePokemon
